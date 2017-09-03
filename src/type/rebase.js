0d11dadd28e423ce4d320734fd79db6a01e7d87c
Derek W. Stavis <dekestavis@gmail.com>
2017-09-03 04:49:57 -0300

    add option to prepend ./ to rebased urls

--- src/type/rebase.js
+++ src/type/rebase.js
@@ -9,13 +9,15 @@ const normalize = require('../lib/paths').normalize;
  * @type {PostcssUrl~UrlProcessor}
  * @param {PostcssUrl~Asset} asset
  * @param {PostcssUrl~Dir} dir
+ * @param {PostcssUrl~Option} options
  *
  * @returns {String|Undefined}
  */
-module.exports = function(asset, dir) {
+module.exports = function(asset, dir, options) {
     const rebasedUrl = normalize(
         path.relative(dir.to, asset.absolutePath)
     );
+    const relative = options.relative ? './' : '';
 
-    return `${rebasedUrl}${asset.search}${asset.hash}`;
+    return `${relative}${rebasedUrl}${asset.search}${asset.hash}`;
 };
